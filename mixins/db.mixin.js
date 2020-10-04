f8febad3c2227b1078afb8fdf4685b2b1ea7de8e
Leonardo Porto <leonardo.porto.souza@gmail.com>
2020-10-04 16:19:42 -0300

    remove logs

--- mixins/db.mixin.js
+++ mixins/db.mixin.js
@@ -8,9 +8,6 @@ const DbService = require("moleculer-db");
 module.exports = collection => {
 	if (process.env.MONGO_URI) {
 		const MongooseAdapter = require("moleculer-db-adapter-mongoose");
-
-		console.log("MONGO URI", process.env.MONGO_URI)
-
 		return {
 			mixins: [DbService],
 			adapter: new MongooseAdapter(process.env.MONGO_URI, {
